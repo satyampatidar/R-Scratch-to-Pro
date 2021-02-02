faca3014f86c5ebd6a3a112900b9efcf33ab68b9
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-02 18:18:59 +0530

    Graphs

---
+++ Graphs Data Visualization.R
@@ -0,0 +1,22 @@
+w=read.csv(file="WDS2012.csv",head=TRUE,sep=",")
+w
+p<-ggplot(data=w,aes(x=le,y=tfr,color=area))
+library(ggplot2)
+p<-ggplot(data=w,aes(x=le,y=tfr,color=area))
+p
+p=ggplot(data=w,aes(x=le,y=tfr,color=area))
+p
+p+geom_point()
+p+geom_jitter()
+p+geom_count()
+
+p <- ggplot(data=w, aes(x=le,y=tfr))
+p+geom_point(size = 3, alpha=1/2)
+p+geom_jitter(size = 4, alpha = 1/2)
+
+wna <- subset(w,region == "Northern Africa")
+p <- ggplot(data = wna, aes(x=le,y=tfr))
+p+geom_point()+geom_text(aes(label=country), nudge_y = .2, size = 4)+xlim(50,80)
+
+p+geom_point()+geom_label(aes(label= country),nudge_y = .3,size=3)+xlim(50,80)+ylim(2,6)
+
