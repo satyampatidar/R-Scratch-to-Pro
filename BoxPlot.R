9428f5888f723415669ce17da6e9251b630519a7
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-03 17:53:30 +0530

    BoxPlot with Median, Annotations

--- BoxPlot.R
+++ BoxPlot.R
@@ -11,11 +11,9 @@ p+geom_boxplot(outlier.size = 0)
 #NoBoxPlot
 p+geom_jitter(position=position_jitter(h=.5))
 
-#Area Chart
 
-ggplot(w,aes(tfr))+geom_area(stat = "bin", bins = 30, fill = "steelblue")
+#BoxPlot with Median, Annotations Inculded, Outliers, Labels included
 
-#BoxPlot
 p <- ggplot(data=subset(w,area=="Africa"),
             aes(x=reorder(factor(region),tfr,FUN=median),y=tfr, color=region))
 p + geom_boxplot(outlier.size=0) + geom_jitter(position=position_jitter(w=.2,h=0))+
@@ -26,8 +24,4 @@ p + geom_boxplot(outlier.size=0) + geom_jitter(position=position_jitter(w=.2,h=0
   labs(title="Country TFR's for Africa, 2012",x="", y="TFR") +
   theme(axis.ticks.x=element_blank(), axis.title.y=element_text(angle=0),
   legend.position="none")
-
-
-
-
 
