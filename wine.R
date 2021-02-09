ccdbbc9b59b83d3680fb6c8eea7f69c62af36a02
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-09 16:54:40 +0530

    Q Plot is now included

--- wine.R
+++ wine.R
@@ -10,3 +10,23 @@ cor(wine$alcohol,wine$quality)
 
 table(wine$quality)
 qplot(wine$fixed.acidity)
+
+#Univariate Plots Section
+
+grid.arrange(qplot(wine$fixed.acidity),
+             qplot(wine$volatile.acidity),
+             qplot(wine$citric.acid),
+             qplot(wine$residual.sugar),
+             qplot(wine$chlorides),
+             qplot(wine$free.sulfur.dioxide),
+             qplot(wine$total.sulfur.dioxide),
+             qplot(wine$density),
+             qplot(wine$pH),
+             qplot(wine$sulphates),
+             qplot(wine$alcohol),
+             qplot(wine$quality),
+             ncol = 4)
+
+p <- ggplot(wine, aes(x = quality, y = volatile.acidity))
+p+facet_grid(.~wine$fixed.acidity)
+p+facet_grid(.~wine$fixed.acidity~.)
