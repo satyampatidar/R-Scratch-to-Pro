170b8975d0bdd0c021a2c808695b722fc03f219f
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-15 16:54:23 +0530

    Final Commit

--- Diamond Dataset.R
+++ Diamond Dataset.R
@@ -13,10 +13,18 @@ View(diamonds)
 # Loading Dimensions
 dim(diamonds)
 
+
+# This DF contains 53940 round cut diamonds.
+# There are 10 variable all are in lowercases.
+# 3 Variables cut, color and clarity are ordered factor arranges in the categorical values in low to high rank order.
+# 6 Variables are in numeric structure carat, depth, table, x, y, z
+# 1 Variable in integer structure that is price.
+
+
 # Plot with legend on the right side gives clarity about each diamond
 x <- ggplot(diamonds, aes(carat,price, color = clarity))
 x+geom_point()
-
+# Layer used
 
 x <- ggplot(diamonds, aes(carat,price, color = cut))
 x+geom_point()
