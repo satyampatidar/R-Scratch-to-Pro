612639cbcd394926308f514741a83653a5fd63dc
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-16 16:51:27 +0530

    Comments added

--- dumbbell charts.R
+++ dumbbell charts.R
@@ -8,17 +8,20 @@ library(gapminder)
 
 # Load Data
 data(gapminder,package = "gapminder")
+
 # Subset Data
 plotdata_long <- filter(gapminder,
                         continent == "Americas" &
-                          year %in% c(1952, 2007)) %>%
+                        year %in% c(1952, 2007)) %>%
   select(country, year, lifeExp)
 
 #convert data to video format
-
 plotdata_wide <- spread(plotdata_long, year, lifeExp)
+
+# Renaming
 names(plotdata_wide) <- c("country","y1952","y2007")
 
 # Create dummbell plot
 
-ggplot(plotdata_wide, aes(y=country, x= y1952, xend = y2007))+ geom_dumbbell()
+ggplot(plotdata_wide, aes(y=country, x= y1952, xend = y2007))+
+  geom_dumbbell()
