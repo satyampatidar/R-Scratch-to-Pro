43222bb0d162a76f1e73662eb1fa7f192356d822
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-16 17:22:58 +0530

    Final Commit

--- area charts.R
+++ area charts.R
@@ -1,3 +1,9 @@
 # Area Charts with Time Series
 
 # Basic area chart on Economics Data
+
+library(ggplot2)
+
+ggplot(economics, aes(x = date, y = psavert))+
+  geom_area(fill="lightblue", color = "black")+
+  labs(title = "Personal Savings Rate", x = "Date", y = "Personal Savings Rate")
