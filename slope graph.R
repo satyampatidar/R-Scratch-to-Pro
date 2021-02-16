54c519e9d8af9ebbcae18981a12865ea6b82f2e8
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-16 17:11:59 +0530

    Final Commit

--- slope graph.R
+++ slope graph.R
@@ -1,2 +1,18 @@
 # Slope Graph
 
+# For limited set of data it is best to use
+
+library(CGPfunctions)
+# Select Central American Countries data for 1992, 1997, 2002, 2007
+df <- gapminder %>%
+  filter(year %in% c(1992,1997,2002,2007) &
+  country %in% c("Panama", "Costa Rica", "Nicaragua",
+                 "Honduras", "EI Salvador","Guatemala","Belize")) %>%
+  mutate(year = factor(year),
+         lifeExp = round(lifeExp))
+
+# Create Slope Graph
+
+newggslopegraph(df, year, lifeExp, country)+
+  labs(title = "Life Expectancy by Country", subtitle = "Central America",
+       caption = "source: gapminder")
