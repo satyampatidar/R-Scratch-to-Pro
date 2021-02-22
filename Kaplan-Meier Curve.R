31148615bb793bc3d5c447c1279b0cbeae22852c
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-22 19:40:34 +0530

    Plotting for Male and Female

--- Kaplan-Meier Curve.R
+++ Kaplan-Meier Curve.R
@@ -11,4 +11,16 @@ sfit <- survfit(Surv(time, status) ~  1, data=lung)
 # Plot of Survival Probability
 
 ggsurvplot(sfit,
-           title="Kaplan-Meier curve for lung cancer survival") 
+           title="Kaplan-Meier curve for lung cancer survival")
+
+#Advance Plot for men and women
+
+sfit <- survfit(Surv(time, status) ~  sex, data=lung)
+ggsurvplot(sfit, 
+           conf.int=TRUE, 
+           pval=TRUE,
+           legend.labs=c("Male", "Female"), 
+           legend.title="Sex",  
+           palette=c("cornflowerblue", "indianred3"), 
+           title="Kaplan-Meier Curve for lung cancer survival",
+           xlab = "Time (days)")
