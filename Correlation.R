4563e65ecb7b944ee4b29fa27c5810941d9c07bd
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-22 19:24:32 +0530

    Graph with Values added

--- Correlation.R
+++ Correlation.R
@@ -1,3 +1,5 @@
+# Loading Libraries
+
 library(ggplot2)
 library(ggcorrplot)
 
@@ -8,7 +10,15 @@ data(SaratogaHouses, package = "mosaicData")
 df <- dplyr::select_if(SaratogaHouses, is.numeric)
 
 # calulate the correlations
+
 r <- cor(df, use="complete.obs")
 round(r,2)
 
 ggcorrplot(r)
+
+# To print Data on Graph
+
+ggcorrplot(r,
+           hc.order = TRUE,
+           type = "lower",
+           lab = TRUE)
