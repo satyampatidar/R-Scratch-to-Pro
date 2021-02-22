d81af7374d4c40b39f8d23f9318e7bb7b5718199
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-22 19:19:37 +0530

    Initial Commit

---
+++ Correlation.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(ggcorrplot)
+
+data(SaratogaHouses, package = "mosaicData")
+
+#select Number Variables
+
+df <- dplyr::select_if(SaratogaHouses, is.numeric)
+
+# calulate the correlations
+r <- cor(df, use="complete.obs")
+round(r,2)
+
+ggcorrplot(r)
