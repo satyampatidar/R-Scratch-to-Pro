f8fd700f11211b5bf9507a3df37a8daa80f3c5e7
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-23 17:17:11 +0530

    Bubble Plot

---
+++ bubble plot.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+library(scatterplot3d)
+
+data(mtcars)
+ggplot(mtcars, aes(x = wt, y = mpg, size = hp))+geom_point()
+# create a bubble plot with modifications
+ggplot(mtcars,        aes(x = wt, y = mpg, size = hp)) +
+  geom_point(alpha = .5,
+             fill="cornflowerblue",
+             color="black",
+             shape=21) +
+  scale_size_continuous(range = c(1, 14)) +
+  labs(title = "Auto mileage by weight and horsepower",
+  subtitle = "Motor Trend US Magazine (1973-74 models)",
+  x = "Weight (1000 lbs)",       
+  y = "Miles/(US) gallon",
+  size = "Gross horsepower")
