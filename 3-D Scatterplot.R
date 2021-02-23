d242fd7ab7bd4980acd46eccb74b290d2ad2283c
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-23 16:31:25 +0530

    Initial Commit

---
+++ 3-D Scatterplot.R
@@ -0,0 +1,20 @@
+# basic 3-D Scatter
+
+library(scatterplot3d)
+
+with(mtcars,{
+  scatterplot3d(x=disp,
+                y= wt,
+                z = mpg,
+                main = "3-D Scatterplot Example1")
+})
+
+with(mtcars,{
+  scatterplot3d(x = disp, y = wt, z = mpg, color = "blue",
+                pch = 19,
+                type = "h", main = "3-D Scatterplot Example",
+                xlab = "Displacement(cu.in.)",
+                ylab = "Weight (lb/1000)",
+                zlab = "Miles/(US) Gallon")
+  
+})
