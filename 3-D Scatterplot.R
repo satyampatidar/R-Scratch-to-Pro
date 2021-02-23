819be42da738de4495b876674dd46e59cee4cf51
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-02-23 17:03:58 +0530

    Error resolved

--- 3-D Scatterplot.R
+++ 3-D Scatterplot.R
@@ -23,13 +23,20 @@ with(mtcars,{
   
 })
 
+
+with(mtcars, {  s3d <- scatterplot3d( x = disp,  y = wt, 
+                                      z = mpg,     color = "blue",     pch = 19,     type = "h",
+                                      main = "3-D Scatterplot Example ",   xlab = "Displacement (cu. in.)",
+                                      ylab = "Weight (lb/1000)",   zlab = "Miles/(US) Gallon")
+
 # convert 3-D coords to 2D projection
 s3d.coords <- s3d$xyz.convert(disp, wt, mpg) 
 
-# plot text with 50% shrink and place to right of points
+# plot text with 50% shrink and pl
 text(s3d.coords$x, 
      s3d.coords$y,   
      labels = row.names(mtcars),  
      cex = .5, 
      pos = 4)
 })
+
