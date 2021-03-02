f12517c7aa90258feb821eb0bbc25943c5d81b04
SATYAM PATIDAR <satyam.patidar@gmail.com>
2021-03-02 16:57:58 +0530

    Dendrogram added

--- heat map.R
+++ heat map.R
@@ -3,3 +3,13 @@ data(mtcars)
 library(superheat)
 superheat(mtcars,scale = TRUE)
 
+# sorted heat map
+# Dendrogram added
+
+superheat(mtcars,
+          scale = TRUE,
+          left.label.text.size = 3,
+          bottom.label.text.size = 3,
+          bottom.label.size = 0.05,
+          row.dendrogram = TRUE)
+
